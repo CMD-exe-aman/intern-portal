bd1110176dbb464c19e51cec02ae34261bd16962
cmd_exe <amankr3252@gmail.com>
2025-08-01 21:01:36 +0530

    Add files via upload
    backend

---
+++ data.js
@@ -0,0 +1,12 @@
+module.exports = {
+  intern: {
+    name: "Aman Kumar",
+    referralCode: "aman2025",
+    donations: 4200
+  },
+  leaderboard: [
+    { name: "Aman Kumar", donations: 4200 },
+    { name: "Riya Mehta", donations: 3000 },
+    { name: "Kunal Singh", donations: 2500 }
+  ]
+};
